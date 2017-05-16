0c2e248bf21a6f428628e02d6ea59c9e5ca7ca08
carbcab <mike.wan9999@gmail.com>
2017-05-15 23:02:04 -0500

    changes

--- carb_db/carb_app/admin.py
+++ carb_db/carb_app/admin.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from carb_app.models import LyftToken
+
+admin.site.register(LyftToken)
 # Register your models here.
--- carb_db/carb_db/settings.py
+++ carb_db/carb_db/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ssdpouh0zwzoyaag6uq-ze#d^$&6eyz*a0g*#aj-wm1gzvr)0$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.67"]
+ALLOWED_HOSTS = ["52.15.168.69"]
 
 
 # Application definition
