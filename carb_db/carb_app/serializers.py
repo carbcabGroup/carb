181872dcc87070d1dcbd9f36618d4be59fee152f
carbcab <mike.wan9999@gmail.com>
2017-05-16 23:36:35 -0500

    uber using wrong model

--- carb_db/carb_app/serializers.py
+++ carb_db/carb_app/serializers.py
@@ -25,7 +25,7 @@ class LyftStatsSerializer(serializers.ModelSerializer):
 class UberStatsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
-        model = LyftStats
+        model = UberStats
         fields = ('id', 'latitude', 'longitude', 'ride_request_time', 'ride_ad_pickup_time', 'ride_pickup_time', 'vehicle_type', 'vehicle_id', 'vehicles_aval', 'cancel_time', 'travel_time', 'arrival_time', 'dest_latitude', 'dest_longitude', 'owner', 'updated')
 
 class UserSerializer(serializers.ModelSerializer):
