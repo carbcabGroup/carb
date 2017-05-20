23f027b425e602d87ffa8aff8e1e717af2407cd0
carbcab <mike.wan9999@gmail.com>
2017-05-20 18:17:32 -0500

    rm default perm settings

--- carb_db/carb_db/settings.py
+++ carb_db/carb_db/settings.py
@@ -52,9 +52,6 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
