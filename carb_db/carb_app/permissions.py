aac1b88fa6eb16455849cd4c59be5651524f19bf
carbcab <mike.wan9999@gmail.com>
2017-05-20 16:51:26 -0500

    rm prints

--- carb_db/carb_app/permissions.py
+++ carb_db/carb_app/permissions.py
@@ -4,16 +4,12 @@ class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = str(request.user)
         if user == 'carbAdmin':
-            print 'ADMIN'
             return True
-        print 'NOT ADMIN'
         return obj.owner == request.user
 
 class IsUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = str(request.user)
         if user == 'carbAdmin':
-            print 'ADMIN'
             return True
-        print 'NOT ADMIN'
         return obj.username == request.user
