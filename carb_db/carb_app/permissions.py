df988ef63737fd5c83ae12e610ccf26e33934526
carbcab <mike.wan9999@gmail.com>
2017-05-20 14:31:19 -0500

    test permissions

--- carb_db/carb_app/permissions.py
+++ carb_db/carb_app/permissions.py
@@ -2,4 +2,6 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user == 'carbAdmin':
+            return True
         return obj.owner == request.user
