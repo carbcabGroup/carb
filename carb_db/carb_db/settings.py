86faf98530a9659179fef351cf1fdd570d919b86
carbcab <mike.wan9999@gmail.com>
2017-05-20 16:46:53 -0500

    added default pagination

--- carb_db/carb_db/settings.py
+++ carb_db/carb_db/settings.py
@@ -56,6 +56,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser',
     ],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
 
