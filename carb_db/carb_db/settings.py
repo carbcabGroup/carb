fa5a8ef9b2c39b5dfc15db2ac9ceecc4446bc11b
carbcab <mike.wan9999@gmail.com>
2017-05-23 20:47:11 -0500

    corsheader added

--- carb_db/carb_db/settings.py
+++ carb_db/carb_db/settings.py
@@ -28,10 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["52.15.168.69", "192.168.1.67", "127.0.0.1"]
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
