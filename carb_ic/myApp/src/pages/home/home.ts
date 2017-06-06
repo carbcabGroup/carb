fc54fc51fa00a395299c4a0e4ec0ed4e3e05d4ef
carbcab <mike.wan9999@gmail.com>
2017-06-05 22:22:53 -0500

    log in page with http get

--- carb_ic/myApp/src/pages/home/home.ts
+++ carb_ic/myApp/src/pages/home/home.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
+import { DTokenProvider } from '../../providers/d-token/d-token';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+    model: any = {};
+    loading = false;
+    error = '';
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, 
+                public dtoken: DTokenProvider) { }
 
-  }
+    ngOnInit() {
+        // reset login status
+        this.dtoken.logout();
+    }
 
+    getToken() {
+        this.dtoken.login(this.model.username, this.model.password)
+            .subscribe(result => {
+                console.log('Login attempt succeeded:');
+                console.log(result);
+                if (result === true) {
+                    console.log('Redirecting to home...');
+                } else {
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
 }
