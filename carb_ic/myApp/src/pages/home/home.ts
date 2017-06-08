f69b1a375f5d0e53fac0264177dc0e6feb70574a
carbcab <mike.wan9999@gmail.com>
2017-06-07 21:25:17 -0500

    login working tho

--- carb_ic/myApp/src/pages/home/home.ts
+++ carb_ic/myApp/src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { DTokenProvider } from '../../providers/d-token/d-token';
 
