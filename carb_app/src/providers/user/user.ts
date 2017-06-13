e9ee67df9947b5035a05030290f83d8557a5ea66
carbcab <mike.wan9999@gmail.com>
2017-06-12 22:09:01 -0500

    added get users

--- carb_app/src/providers/user/user.ts
+++ carb_app/src/providers/user/user.ts
@@ -1,25 +1,69 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
-
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw'
 /*
-  Generated class for the UserProvider provider.
+    Generated class for the UserProvider provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
+    See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+    for more info on providers and Angular 2 DI.
 */
+
+export interface User {
+    id: number;
+    username: string;
+    lyft_token: number[];
+    uber_token: number[];
+}
+
 @Injectable()
 export class UserProvider {
 
-  public auth: string;
+    public auth: string;
 
-  constructor(public http: Http) {
-    console.log('user provider'); 
-  }
+    constructor(public http: Http) {
+        console.log('user provider'); 
+    }
 
-  getUsers() {
-    this.auth = localStorage.getItem('currentUser')
-    console.log(JSON.parse(this.auth).token);
-  }
+    getUsers() {
+        this.auth = localStorage.getItem('currentUser')
+        console.log(JSON.parse(this.auth).token);
+        let urlbase = 'http://13.58.151.236:8088';
+        let headers = new Headers({ 'Authorization': 'JWT ' + JSON.parse(this.auth).token });
+        let options = new RequestOptions({ headers: headers });
 
+        // get user overview
+        console.log('Getting user details...');
+        let path = '/users';
+        let url = urlbase + path;
+        let userResults = this.http.get(url, options).map(mapUserResp).catch(this.handleError);
+        console.log('User info:');
+        console.log(userResults);
+        return userResults;
+    }
+
+    private handleError(error: any) {
+        let msg = error.message || "Error accessing API.";
+        console.error(msg);
+        return Observable.throw(msg);
+    }
+}
+
+// Static helpers
+function mapUserResp(r: Response): User[]{
+    console.log('Mapping a user API response...');
+    return r.json().results.map(toUser);
+}
+
+function toUser(r: any): User{
+    let user = <User>({
+        id: r.id,
+        username: r.username,
+        lyft_token: r.lyft_token,
+        uber_token: r.uber_token,
+    });
+    console.log('Parsed user:', user);
+    return user;
 }
